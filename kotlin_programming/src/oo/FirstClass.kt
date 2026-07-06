7638606c174462084ecadc6ccd6ee961c8d0a854
Josaf4 <josafa.soares2909@gmail.com>
2026-07-06 13:31:49 -0400

    Object-Oriented Programming - Part I - Class and Methods

---
+++ kotlin_programming/src/oo/FirstClass.kt
@@ -0,0 +1,31 @@
+package oo
+
+class Person {
+    var name: String = "Sarah"
+    var age: Int = 42
+
+    fun speak() {
+        println("Hello!")
+    }
+
+    fun greet(name: String) {
+        println("Hello $name")
+    }
+
+//    fun getYearOfBirth(): Int {
+//        return 2016 - age
+//    }
+
+    fun getYearOfBirth(): Int = 2016 - age
+}
+
+fun main(args: Array<String>) {
+    val person = Person()
+
+    person.speak()
+    person.greet("world")
+    println(person.getYearOfBirth())
+
+    println(person.name)
+    println(person.age)
+}
