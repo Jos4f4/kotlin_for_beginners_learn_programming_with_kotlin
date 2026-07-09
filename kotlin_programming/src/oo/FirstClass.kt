fde1ab5c377ec2c0c92802c65bb25b30b9aac94c
Josaf4 <josafa.soares2909@gmail.com>
2026-07-09 10:24:43 -0400

    Abstract vs Open

--- kotlin_programming/src/oo/FirstClass.kt
+++ kotlin_programming/src/oo/FirstClass.kt
@@ -1,12 +1,10 @@
 package oo
 
-open class Person(open val name: String, open var age: Int) {
+abstract class Person(open val name: String, open var age: Int) {
 
-    init { println("Object was created") }
+    abstract fun speak()
 
-    fun speak() { println("Hello!") }
-
-    fun greet(name: String) { println("Hello $name") }
+    open fun greet(name: String) { println("Hello $name") }
 
     fun getYearOfBirth(): Int = 2016 - age
 }
@@ -14,11 +12,17 @@ open class Person(open val name: String, open var age: Int) {
 class Student(override val name: String, override var age: Int, val studentID: Long): Person(name, age) {
 
     fun isIntelligent() = true
+
+    override fun speak() { println("Hi there, I'm a student!") }
+
+    override fun greet(name: String) { println("Howdy $name") }
 }
 
 class Employee(override val name: String, override var age: Int): Person(name, age) {
 
     fun receivePayment() { println("Received payment.") }
+
+    override fun speak() { println("Hi I'm an employee") }
 }
 
 fun main(args: Array<String>) {
@@ -26,7 +30,10 @@ fun main(args: Array<String>) {
     val student = Student("John", 25, 3647284)
     student.speak()
 
+    println(student.isIntelligent())
+
     val employee = Employee("Mary", 32)
     employee.getYearOfBirth()
     employee.receivePayment()
+    employee.speak()
 }
