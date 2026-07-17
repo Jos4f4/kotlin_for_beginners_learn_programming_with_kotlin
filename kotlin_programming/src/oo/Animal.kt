7c9d8fc56c0c803bc1ae143b96d0879e702384ed
Josaf4 <josafa.soares2909@gmail.com>
2026-07-17 17:32:57 -0400

    Visibilities and Generics Stacks / Functions

--- kotlin_programming/src/oo/Animal.kt
+++ kotlin_programming/src/oo/Animal.kt
@@ -1,15 +1,18 @@
 package oo
 
-class Animal {
-    var age: Int = 0
+open class Animal {
+    private var age: Int = 0
+    protected var name = "Sam"
+    internal val isDangerous = true
 
-    get() = field
-    set(value) { if (value >= 0) { field = value } }
+    public fun isOld(): Boolean { return age > 12 }
+
+}
+
+class Vertebrate: Animal() {
+    fun introduceYourself() { println(this.name) }
 }
 
 fun main(args: Array<String>) {
     val animal = Animal()
-    animal.age = 8
-    animal.age = -2
-    println(animal.age)
 }
